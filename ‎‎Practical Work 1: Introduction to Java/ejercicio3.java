b637b7f40ba9c4146ece802b3c2f38d79aea154a
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-08-13 16:28:19 -0300

    Update ejercicio3.java

--- ‎‎Practical Work 1: Introduction to Java/ejercicio3.java
+++ ‎‎Practical Work 1: Introduction to Java/ejercicio3.java
@@ -1,1 +1,16 @@
 
+package ejercicio3;
+
+
+public class ejercicio3 {
+
+
+    public static void main(String[] args) {
+        String nombre = "Martin";
+        int edad = 21;
+        double altura = 1.80;
+        boolean estudiante = true;
+        System.out.println(nombre + " tiene " + edad + " años y mide " + altura + ", su condicion de estudiante es: " + estudiante);
+    }
+
+}
