ea42cdc86fd21a5f21cf2f4fc7342f3334eff7b0
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-09-26 22:48:17 -0300

    Update ejer4_main.java

--- Practical work 3: introduction to object-oriented programming/ejer4_main.java
+++ Practical work 3: introduction to object-oriented programming/ejer4_main.java
@@ -1,40 +1,25 @@
 package trabajo_practico_3;
 
 
-public class Libro {
-    private String titulo;
-    private String autor;
-    private int anioPublicacion;
+public class principal_granja {
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getAnioPublicacion() {
-        return anioPublicacion;
-    }
-
-    public void setAnioPublicacion(int anio) {
-        if (anio>=0){
-        anioPublicacion = anio;
-    }
+    
+    public static void main(String[] args) {
+        Gallina gallina1= new Gallina(), gallina2 = new Gallina();
+        gallina1.setIdGallina(992);
+        gallina2.setIdGallina(945);
+        gallina1.setEdad(2);
+        gallina2.setEdad(1);
+        gallina1.setHuevosPuestos(200);
+        gallina2.setHuevosPuestos(120);
+        gallina1.mostrarEstado();
+        gallina2.mostrarEstado();
+        gallina1.envejecer(1);
+        gallina1.ponerHuevo(110);
+        gallina2.envejecer(1);
+        gallina2.ponerHuevo(200);
+        gallina1.mostrarEstado();
+        gallina2.mostrarEstado();
     }
     
-    public void mostrarLibro(){
-            System.out.println(titulo);
-            System.out.println(autor);
-            System.out.println(anioPublicacion);
-}
 }
