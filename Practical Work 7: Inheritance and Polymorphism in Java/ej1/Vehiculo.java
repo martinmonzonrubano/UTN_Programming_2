03cf6cc7038e6f7434d2457824c39b883f0e2ed5
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-21 14:23:23 -0300

    Create Vehiculo.java

---
+++ Practical Work 7: Inheritance and Polymorphism in Java/ej1/Vehiculo.java
@@ -0,0 +1,35 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 21 nov 2025
+ * Clase: Vehiculo
+ */
+package ejer3;
+
+public class Vehiculo {
+    private final String marca;
+    private final String modelo;
+    private final int anio;
+
+    public Vehiculo(String marca, String modelo, int anio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    // Muestra info genérica del vehículo
+    public String mostrarInfo() {
+        return marca + " " + modelo + " (" + anio + ")";
+    }
+}
