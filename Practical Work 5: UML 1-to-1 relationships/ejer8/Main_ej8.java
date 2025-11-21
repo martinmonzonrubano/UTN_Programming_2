0ea4fee2490ac6c34df162ad2ef6124d4d2fe0b0
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-20 21:29:21 -0300

    Create Main_ej8.java

---
+++ Practical Work 5: UML 1-to-1 relationships/ejer8/Main_ej8.java
@@ -0,0 +1,28 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 20 nov 2025
+ * Clase: Main_ej8
+ */
+
+package ej8;
+
+public class Main_ej8 {
+
+    public static void main(String[] args) {
+
+        // Creo un usuario que firmará el documento
+        Usuario usuarioFirmante = new Usuario("Ricardo Diaz", "rdiaz@gmail.com");
+       
+        // Creo el documento y dentro creo su FirmaDigital
+        Documento contrato = new Documento(
+            "Contrato Laboral", 
+            "El presente documento establece:", 
+            "RDiaZ",
+            "27-09-2025", 
+            usuarioFirmante 
+        );
+        
+        // Muestro toda la información
+        contrato.mostrarInfoCompleta();
+    }
+}
