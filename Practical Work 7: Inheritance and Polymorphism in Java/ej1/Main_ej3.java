40b810c0e901b24cb4cf2dd783e64fe632128ae8
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-21 14:23:05 -0300

    Create Main_ej3.java

---
+++ Practical Work 7: Inheritance and Polymorphism in Java/ej1/Main_ej3.java
@@ -0,0 +1,22 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 21 nov 2025
+ * Clase: Main_ej3
+ */
+package ejer3;
+
+public class Main_ej3 {
+
+    public static void main(String[] args) {
+
+        // Creo varios autos para probar
+        Auto auto1 = new Auto("Honda", "Civic", 2024, 4, "Sport");
+        Auto auto2 = new Auto("Toyota", "Corolla", 2025, 4, "Full");
+        Auto auto3 = new Auto("Ford", "Mustang", 2023, 2, "GT");
+
+        // Muestro la info de cada auto
+        System.out.println(auto1.mostrarInfo());
+        System.out.println(auto2.mostrarInfo());
+        System.out.println(auto3.mostrarInfo());
+    }
+}
