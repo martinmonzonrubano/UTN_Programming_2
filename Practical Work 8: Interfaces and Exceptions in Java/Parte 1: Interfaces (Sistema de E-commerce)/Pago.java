c4252f82ca2f3630048793a4545c69fe79576675
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-22 11:27:15 -0300

    Create Pago.java

---
+++ Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/Pago.java
@@ -0,0 +1,11 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 22 nov 2025
+ * Clase: Pago
+ */
+package ejer8;
+
+// Comportamiento general de un medio de pago
+public interface Pago {
+    void procesarPago(double monto);
+}
