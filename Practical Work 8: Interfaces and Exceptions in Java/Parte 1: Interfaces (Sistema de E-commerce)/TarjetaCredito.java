840eef35fe5492c9b3393163c4e659e1d2c94f8c
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-22 11:28:30 -0300

    Create TarjetaCredito.java

---
+++ Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/TarjetaCredito.java
@@ -0,0 +1,20 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 22 nov 2025
+ * Clase: TarjetaCredito
+ */
+package ejer8;
+
+// Pago con tarjeta que aplica un 10% de descuento
+public class TarjetaCredito implements Pago, PagoConDescuento {
+
+    @Override
+    public double aplicarDescuento(double monto) {
+        return monto * 0.90;
+    }
+
+    @Override
+    public void procesarPago(double monto) {
+        System.out.println("Pagando con tarjeta... Monto final: $" + monto);
+    }
+}
