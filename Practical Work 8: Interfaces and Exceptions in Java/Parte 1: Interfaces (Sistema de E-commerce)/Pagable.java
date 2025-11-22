3c63ca19754726026cf017450621b2186e06204a
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-22 11:26:04 -0300

    Create Pagable.java

---
+++ Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/Pagable.java
@@ -0,0 +1,11 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 22 nov 2025
+ * Clase: Pagable
+ */
+package ejer8;
+
+// Contrato para cosas que saben calcular un total
+public interface Pagable {
+    double calcularTotal();
+}
