bfb2c80de940023021edd996ef870a5af0b24267
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-22 11:32:27 -0300

    Create Notificable.java

---
+++ Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/Notificable.java
@@ -0,0 +1,11 @@
+/**
+ * Autor: Martin Monzon
+ * Fecha: 22 nov 2025
+ * Clase: Notificable
+ */
+package parte1;
+
+// Contrato para objetos que pueden recibir notificaciones
+public interface Notificable {
+    void notificar(String mensaje);
+}
