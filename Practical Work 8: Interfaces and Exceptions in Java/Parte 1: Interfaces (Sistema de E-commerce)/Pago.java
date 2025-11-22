331ea9940c51e35f6a82e0321d0a3f668a521ec1
Martin Monzon Rubano <martinmonzonfacultad@gmail.com>
2025-11-22 11:31:21 -0300

    Update Pago.java

--- Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/Pago.java
+++ Practical Work 8: Interfaces and Exceptions in Java/Parte 1: Interfaces (Sistema de E-commerce)/Pago.java
@@ -3,9 +3,10 @@
  * Fecha: 22 nov 2025
  * Clase: Pago
  */
-package ejer8;
+package parte1;
 
 // Comportamiento general de un medio de pago
 public interface Pago {
     void procesarPago(double monto);
 }
+
